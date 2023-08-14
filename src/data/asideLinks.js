2910f54bb8c1dc2823fd2e9285acf316c627d469
Allan Raj <allanrajcrp@gmail.com>
2023-08-14 13:51:36 +0530

    form spree email

---
+++ src/data/asideLinks.js
@@ -0,0 +1,31 @@
+export const standardLinks = [
+    {
+        name: "ISO 9001",
+        link: "#9001"
+    },
+    {
+        name: "ISO 27001",
+        link: "#27001"
+    },
+    {
+        name: "ISO 14001",
+        link: "#14001"
+    },
+    {
+        name: "ISO 45001",
+        link: "#45001"
+    },
+    {
+        name: "ISO/FSSC 22000 ",
+        link: "#2200"
+    },
+    {
+        name: "ISO 13485",
+        link: "#13485"
+    },
+    {
+        name: "ISO 17025",
+        link: "#17025"
+    },
+    
+]
---
+++ src/data/qmsCourseCard.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    name: "QMS ISO 9001:2015 Fundamentals Training Course | CQI IRCA Certified",
+    description:
+      "The focus of our CQI & IRCA Certified Online ISO 9001:2015 Fundamentals Training Course is to comprehensively introduce Learners to Quality Management Systems and provide them with a highly insightful introduction to the purpose, intent and requirements of ISO 9001:2015 plus the business case for adopting ISO 9001. It can be completed anytime, anywhere - fully online or on the go!",
+    duration: "1 day",
+    deliveryMethods: "Online, Virtual, Classroom",
+    certifier: "cqi",
+    cqiID: "896768",
+  },
+  {
+    name: "QMS ISO 9001:2015 Internal Auditor Training Course | CQI IRCA Certified",
+    description:
+      "The focus of the Kelmac Group® ISO 9001:2015 Internal Auditor Training Course is on assisting and verifying the competencies & capabilities of the Learner to be able to effectively audit a process, activity or department/function of a QMS based on ISO 9001:2015 requirements with specific emphasis of 1st Party auditing [Internal audits].",
+    duration: "2 days",
+    deliveryMethods: "Virtual, Classroom",
+    certifier: "cqi",
+    cqiID: "896768",
+  },
+
+  {
+    name: "QMS ISO 9001:2015 Lead Auditor Training Course | CQI IRCA Certified",
+    description:
+      "This CQI and IRCA Certified ISO 9001:2015 Auditor/Lead Auditor Training Course is based on assisting and verifying the competencies/capabilities of the Learner to be able to effectively audit a complete QMS based on ISO 9001:2015 requirements with specific emphasis of 2nd Party auditing [Customer/Supplier audits] and 3rd Party auditing [Certification Bodies].",
+    duration: "5 days",
+    deliveryMethods: "Virtual, Classroom",
+    certifier: "cqi",
+    cqiID: "896768",
+  },
+];
